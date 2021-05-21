6f36101dc6ddf1752493b33975b7f17774790f04
rkdntm1 <rkdntm1@gmail.com>
2021-05-21 18:32:08 +0900

    board & post & insert

---
+++ bb002_post/src/main/java/www/dream/com/bulletinBoard/persistence/PostMapper.java
@@ -0,0 +1,17 @@
+package www.dream.com.bulletinBoard.persistence;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+
+import www.dream.com.bulletinBoard.model.BoardVO;
+import www.dream.com.bulletinBoard.model.PostVO;
+
+public interface PostMapper {
+	//LRCUD
+	/* mapper 함수의 인자 개수가 여러개 일 경우 필수적으로 @Param을 넣야합니다.
+	 * 이를 실수하지 않기 위하여 한개여도 그냥 명시적으로 넣어주자 */
+	public List<PostVO> getList(@Param("boardId") int boardId);
+	
+	public int insert(@Param("board") BoardVO board, @Param("post") PostVO post);
+}
---
+++ bb002_post/target/classes/www/dream/com/common/dictionarySeed.sql
@@ -0,0 +1,17 @@
+truncate table s_id_seed;
+drop table s_id_seed;
+
+create table s_id_seed(
+	seq_id		number(19)		primary key,
+	seed 		char(5)
+);
+
+CREATE OR REPLACE FUNCTION get_id(intSeed number) RETURN char
+IS
+	ret char(5);
+BEGIN
+	select seed into ret from s_id_seed where seq_id = intSeed;
+	RETURN ret;
+END;
+
+select get_id(15) from dual;

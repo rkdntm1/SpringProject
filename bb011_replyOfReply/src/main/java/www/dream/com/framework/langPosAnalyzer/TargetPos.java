443c624f6f08bc4e8bb094391053af6b83ad16f9
rkdntm1 <50398536+rkdntm1@users.noreply.github.com>
2021-06-18 02:23:51 +0900

    Delete bb011_replyOfReply directory

--- bb011_replyOfReply/src/main/java/www/dream/com/framework/langPosAnalyzer/TargetPos.java
+++
@@ -1,6 +0,0 @@
-package www.dream.com.framework.langPosAnalyzer;
-
-public enum TargetPos {
-	//일반명사, 고유명사, 영단어, 한자
-	NNG, NNP, SL, SH;
-}
--- bb011_replyOfReply/src/main/java/www/dream/com/framework/util/ToStringSuperHelp.java
+++
@@ -1,7 +0,0 @@
-package www.dream.com.framework.util;
-
-public class ToStringSuperHelp {
-	public static String trimSuperString(String str) {
-		return str.substring(str.indexOf('[') + 1, str.lastIndexOf(']'));
-	}
-}
